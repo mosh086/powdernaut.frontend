b672385777eaff8b4b13b2498c298d84dfa70fbd
Edilberto Salazar <mosh086@gmail.com>
2026-02-18 11:28:56 -0600

    feat: Init

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { LayoutComponent } from '@layouts/layout/layout.component';
+
+export const routes: Routes = [
+    { path: '', component: LayoutComponent },
+];
+    
---
+++ src/app/modules/dashboard/components/main/main.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pn-main',
+  templateUrl: './main.html',
+  styleUrl: './main.scss',
+  standalone: false
+})
+export class MainComponent {
+
+}
